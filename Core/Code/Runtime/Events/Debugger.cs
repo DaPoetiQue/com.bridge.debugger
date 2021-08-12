7870bcd89192af91667acd6a0497a449f03919fb
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2021-08-12 14:20:01 +0200

    Feat : Added runtime debugger.

---
+++ Core/Code/Runtime/Events/Debugger.cs
@@ -0,0 +1,30 @@
+// Namespace.
+namespace RedicalGamez.Dev.ToolKit
+{
+    public class Debugger
+    {
+        #region Delegates
+
+        public delegate void DebugEvent<T,C,M>(T logType, C logClassName, M logMessage);
+
+        #endregion
+
+        #region Events
+
+        public static event DebugEvent<DebugData.LogType, object, object> AddListener;
+
+        #endregion
+
+        #region Invokes
+
+        public static void Log(DebugData.LogType logType, object logClassName, object logMessage)
+        {
+            if(AddListener != null)
+            {
+                AddListener(logType, logClassName, logMessage);
+            }
+        }
+
+        #endregion
+    }
+}
